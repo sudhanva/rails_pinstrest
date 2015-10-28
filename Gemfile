3f5379066e778064fdec31af14a37742b90bcad5
S.Shivangere <sudhanva.sa@gmail.com>
2015-10-28 10:34:24 +0000

    Added devise/ user associations

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'simple_form'
+gem 'devise', '~> 3.5', '>= 3.5.2'
 group :development, :test do
   gem 'byebug'
 end
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,12 +9,12 @@ class PinsController < ApplicationController
     end
     
     def new
-        @pin=Pin.new
+        @pin=current_user.pins.build
     end
     
     
     def create
-        @pin= Pin.new(pin_params)
+        @pin= current_user.pins.build(pin_params)
         if @pin.save
             redirect_to @pin, notice: "Successfully created the new pin"
         else
