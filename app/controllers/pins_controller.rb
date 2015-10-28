07bacf3d82dea60adcac4845fa1b06f5572cd892
S.Shivangere <sudhanva.sa@gmail.com>
2015-10-28 11:37:05 +0000

    Add paperclip gem for image upload

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -43,7 +43,7 @@ class PinsController < ApplicationController
     private 
     
     def pin_params
-        params.require(:pin).permit(:title,:description)
+        params.require(:pin).permit(:title,:description,:image)
     end
     
     def find_pin
